8cf92c870564bdd6c10549bc2e2193e384cd2e87
gehan <gehan0423@naver.com>
2025-01-08 11:39:17 +0900

    Lecture00, 05, 99

---
+++ ch99_homework/01_simple_sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// 3개의 수를 입력받고, 큰 숫자로 정렬해서 출력하는 프로그램
+
+int main() {
+    int num1 = 20, num2 = 10, num3 = 50;  // 고정
+
+
+    printf("%d > %d > %d", num1, num2, num3);  // 고정
+
+
+}
