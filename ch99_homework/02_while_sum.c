070e193fe12f6f12e299b61f52e16c4e8be0eddb
gehan <gehan0423@naver.com>
2025-01-10 14:16:40 +0900

    Update Lecture ch02, 99

--- ch99_homework/02_while_sum.c
+++ ch99_homework/02_while_sum.c
@@ -3,18 +3,20 @@
 // 문제: while문을 사용해서 1~100까지의 값을 홀수합, 짝수합으로 계산하세요.
 
 int main() {
-    int i, n;
-    int evenSum = 0; 
+    int i=1;
+    int evenSum = 0;
     int oddSum = 0;
 
-    while(1<=n<=100) {
-        if(n%2==0) {
-            evenSum += n;
+    while(i<=100) {
+        if(i%2==0) {
+            evenSum += i;
         }
         else {
-            
+            oddSum += i;
         }
-        
+
         i += 1;
     }
+
+    printf("짝수합: %d, 홀수합: %d", evenSum, oddSum);
 }
