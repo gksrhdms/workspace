885c4f0de0cb827399692ba61cd707d2a32f3beb
gehan <gehan0423@naver.com>
2025-01-21 09:52:22 +0900

    license

--- final_practice/if_problem.c
+++ final_practice/if_problem.c
@@ -18,11 +18,28 @@
 
 // 2. 0~100점을 입력 받고, 학점으로 변환해서 출력
 
-int main() {
-    int score;
-    printf("점수를 입력하세요: ");
-    scanf("%d", &score);
+// int main() {
+//     int score;
+//     printf("점수를 입력하세요: ");
+//     scanf("%d", &score);
 
     
-}
+// }
 // 3. 두 수를 입력받고 크기를 비교하는 프로그램
+
+int main() {
+    int a, b;
+    printf("비교할 두 수를 입력하세요: ");
+    scanf("%d %d", &a, &b);
+
+    if(a>b) {
+        printf("%d가 더 큽니다.\n", a);
+    }
+    else if(a==b) {
+        printf("%d와 %d는 같습니다.\n", a, b);
+    }
+    else {
+        printf("%d가 더 큽니다.\n", b);
+    }
+}
+
